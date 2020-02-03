5499c2b73772688cea569a93f6f253778a8d699b
vk16309 <43472427+vk16309@users.noreply.github.com>
2020-02-03 23:21:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ app.use(function(err,req,res,next){
 	res.status(422).send({error: err.message});
 });
 
-app.listen(process.env.port||4000,function(){
+app.listen(process.env.PORT||4000,function(){
 	console.log('now listening to request');
 });
